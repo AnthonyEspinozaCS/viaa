a133a1c756575ebebea9afe57701a503e43389b8
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-07-12 15:53:46 -0700

    First draft.

--- src/nav.js
+++ src/nav.js
@@ -17,12 +17,8 @@ function myFunction(number) {
   const bio = document.getElementById(`more-${number}`);
   const btnText = document.getElementById(`button-${number}`);
   const dots = document.getElementById(`dots-${number}`);
-  const background = document.getElementById(`p-background-${number}`);
-  console.log(number);
   bio.classList.toggle("hidden");
   dots.classList.toggle("hidden");
-  background.classList.toggle("hidden");
-  background.classList.toggle("d-block");
   if (btnText.innerText === "Read More") btnText.innerText = "Read Less";
   else btnText.innerText = "Read More";
 }
